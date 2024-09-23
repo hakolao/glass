370fce8820eb8f8b71d5d4217bc0fb0beac64739
Okko Hakola <okkohakola@gmail.com>
2024-09-23 20:11:22 +0300

    Add silly out of bounds bug repro

--- examples/shader_with_includes/main.rs
+++ examples/shader_with_includes/main.rs
@@ -1,13 +1,13 @@
-use std::{borrow::Cow, collections::HashMap, path::PathBuf};
+use std::{borrow::Cow, path::PathBuf};
+use flume::{Receiver, unbounded};
 
 use glass::{
-    utils::ShaderModule, Glass, GlassApp, GlassConfig, GlassContext, GlassError, RenderData,
+    Glass, GlassApp, GlassConfig, GlassContext, GlassError, RenderData,
 };
-use wgpu::{
-    CommandBuffer, MultisampleState, PipelineLayoutDescriptor, PrimitiveState, RenderPipeline,
-    RenderPipelineDescriptor, ShaderModuleDescriptor, StoreOp, TextureFormat,
-};
+use wgpu::{CommandBuffer, Device, MultisampleState, PipelineLayout, PipelineLayoutDescriptor, PrimitiveState, RenderPipeline, RenderPipelineDescriptor, ShaderModuleDescriptor, StoreOp, TextureFormat};
+use wgpu::naga::Module;
 use winit::event_loop::ActiveEventLoop;
+use glass::utils::WatchedShaderModule;
 
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
@@ -21,11 +21,41 @@ fn main() -> Result<(), GlassError> {
 #[derive(Default)]
 struct TriangleApp {
     triangle_pipeline: Option<RenderPipeline>,
+    layout: Option<PipelineLayout>,
+    shader: Option<WatchedShaderModule>,
 }
 
 impl GlassApp for TriangleApp {
     fn start(&mut self, _event_loop: &ActiveEventLoop, context: &mut GlassContext) {
-        self.triangle_pipeline = Some(create_triangle_pipeline(context));
+        let (layout, pipeline, shader) = create_shader_and_pipeline(context);
+        self.triangle_pipeline = Some(pipeline);
+        self.layout = Some(layout);
+        self.shader = Some(shader);
+    }
+
+    fn end_of_frame(&mut self, context: &mut GlassContext) {
+        if let Some(shader) = &mut self.shader {
+            let changed_paths = shader.changed_paths();
+            if !changed_paths.is_empty() {
+                println!("Changed\n{:#?}", changed_paths);
+                shader.reload().unwrap();
+                // Hot reload
+                let (new_pipeline, rx) = create_pipeline_uncaptured(
+                    context.device(),
+                    &self.layout.as_ref().unwrap(),
+                    shader.module().unwrap().into(),
+                    |_device, layout, module| {
+                        create_pipeline(context, layout, module)
+                    },
+                );
+                if let Ok(err) = rx.try_recv() {
+                    println!("Pipeline Creation Failed: {}", err);
+                } else {
+                    self.triangle_pipeline = Some(new_pipeline);
+                    println!("Reloaded triangle pipeline");
+                }
+            }
+        }
     }
 
     fn render(
@@ -62,40 +92,12 @@ impl GlassApp for TriangleApp {
     }
 }
 
-fn create_triangle_pipeline(context: &GlassContext) -> RenderPipeline {
+fn create_shader_and_pipeline(context: &GlassContext) -> (PipelineLayout, RenderPipeline, WatchedShaderModule) {
     // Dynamic includes
-    let _shader_module = ShaderModule::new(&PathBuf::from(
+    let shader_module = WatchedShaderModule::new(&PathBuf::from(
         "examples/shader_with_includes/triangle_with_include.wgsl",
     ))
-    .unwrap();
-
-    // Static includes
-    let mut static_includes = HashMap::default();
-    // Include all files that you wish to refer to in your root shader. Tedious, but this ensures
-    // You can keep using includes while containing static shaders.
-    static_includes.insert(
-        "examples/shader_with_includes/triangle_with_include.wgsl",
-        include_str!("triangle_with_include.wgsl"),
-    );
-    static_includes.insert(
-        "examples/shader_with_includes/consts.wgsl",
-        include_str!("consts.wgsl"),
-    );
-    static_includes.insert(
-        "examples/triangle/triangle.wgsl",
-        include_str!("../triangle/triangle.wgsl"),
-    );
-    let shader_module = ShaderModule::new_with_static_sources(
-        "examples/shader_with_includes/triangle_with_include.wgsl",
-        &static_includes,
-    )
-    .unwrap();
-    let shader = context
-        .device()
-        .create_shader_module(ShaderModuleDescriptor {
-            label: None,
-            source: wgpu::ShaderSource::Naga(Cow::Owned(shader_module.into())),
-        });
+        .unwrap();
     let layout = context
         .device()
         .create_pipeline_layout(&PipelineLayoutDescriptor {
@@ -103,11 +105,23 @@ fn create_triangle_pipeline(context: &GlassContext) -> RenderPipeline {
             bind_group_layouts: &[],
             push_constant_ranges: &[],
         });
+    let pipeline = create_pipeline(context, &layout, shader_module.module().unwrap().into());
+    (layout, pipeline, shader_module)
+}
+
+fn create_pipeline(context: &GlassContext, layout: &PipelineLayout, module: Module) -> RenderPipeline {
+    let shader = context
+        .device()
+        .create_shader_module(ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Naga(Cow::Owned(module)),
+        });
+
     let pipeline = context
         .device()
         .create_render_pipeline(&RenderPipelineDescriptor {
             label: None,
-            layout: Some(&layout),
+            layout: Some(layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
@@ -127,4 +141,19 @@ fn create_triangle_pipeline(context: &GlassContext) -> RenderPipeline {
             cache: None,
         });
     pipeline
+}
+
+pub fn create_pipeline_uncaptured<T>(
+    device: &Device,
+    layout: &PipelineLayout,
+    shader: Module,
+    create_pipeline_fn: impl Fn(&Device, &PipelineLayout, Module) -> T,
+) -> (T, Receiver<wgpu::Error>) {
+    let (tx, rx) = unbounded::<wgpu::Error>();
+    device.on_uncaptured_error(Box::new(move |e: wgpu::Error| {
+        tx.send(e).expect("sending shader hot reload error failed");
+    }));
+    let p = create_pipeline_fn(device, layout, shader);
+    device.on_uncaptured_error(Box::new(|e| panic!("{}", e)));
+    (p, rx)
 }
--- examples/shader_with_includes/triangle_with_include.wgsl
+++ examples/shader_with_includes/triangle_with_include.wgsl
@@ -1,2 +1,6 @@
 #include examples/shader_with_includes/consts.wgsl
 #include examples/triangle/triangle.wgsl
+
+fn test_function(some_vec: vec2<f32>) -> vec3<f32> {
+    return vec3<f32>(some_vec, 1.0);
+}
