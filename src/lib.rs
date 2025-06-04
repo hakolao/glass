15c5c8ac5a75d0c6e44c60f43772a96cbb53240e
Okko Hakola <okkohakola@gmail.com>
2025-06-04 13:57:45 +0300

    Remove egui from deps

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,6 @@ pub mod texture;
 pub mod utils;
 pub mod window;
 
-// For convenience, export egui libs when that feature is enabled
-#[cfg(feature = "egui_gui")]
-pub use egui;
-#[cfg(feature = "egui_gui")]
-pub use egui_demo_lib;
-#[cfg(feature = "egui_gui")]
-pub use egui_extras;
-#[cfg(feature = "egui_gui")]
-pub use egui_wgpu;
-#[cfg(feature = "egui_gui")]
-pub use egui_winit;
-// --
 pub use image;
 pub use wgpu;
 pub use winit;
